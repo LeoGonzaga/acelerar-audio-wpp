d38b6b414083766bd46e7946cd4010c154cfc95a
Alessandro Dias <ale.dias@hotmail.com.br>
2021-03-25 14:25:57 -0300

    Adding select with some speed options

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const awaitDiv = setInterval(() => {
-  const header = document.querySelector("._1R3Un");
+  const header = document.querySelector("header._1R3Un");
+
   if (header) {
-    console.log(header);
     clearInterval(awaitDiv);
-    const btn = document.createElement("button");
-    btn.innerHTML = "2x";
-    btn.addEventListener("click", () => {
+
+    const selectItems = [
+      { text: '1x', value: 1 },
+      { text: '1.25x', value: 1.25 },
+      { text: '1.5x', value: 1.5 },
+      { text: '1.75x', value: 1.75 },
+      { text: '2x', value: 2 },
+    ];
+
+    const speedSelector = document.createElement("select");
+    speedSelector.style.width = "40px";
+
+    for (let i = 0; i < selectItems.length; i++) {
+      const option = document.createElement("option");
+      option.text = selectItems[i].text;
+      option.value = selectItems[i].value;
+      speedSelector.appendChild(option);
+    }
+
+    speedSelector.addEventListener("change", (event) => {
+      const speed = event.target.value;
       const audios = document.querySelectorAll("audio");
-      audios.forEach((audio) => (audio.playbackRate = 2));
+      audios.forEach((audio) => (audio.playbackRate = speed));
     });
-    header.appendChild(btn);
+
+    header.appendChild(speedSelector);
   }
 }, [1000]);
